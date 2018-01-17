75873e957252abd993d5651b19a76586008186c3
Pixie <32332642+pixiekidd@users.noreply.github.com>
2018-01-17 19:19:14 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,5 @@
 module.exports.run = async(bot,msg,args) =>{
-const Discord = require('discord.js');
-const RichEmbed = new Discord.RichEmbed();
 
-
-var ping = Math.round(bot.ping);
-var embed = new Discord.RichEmbed()
-.setTitle('Pong! :ping_pong: ')
-.setColor('RANDOM')
-.setDescription(' Ping is `'+ping+'` ms')
-
-
-msg.channel.send(embed);
 }
 
 
