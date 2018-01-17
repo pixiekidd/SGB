4de3cb7fa54ee9f85101bb48d92f3110cc7c0c5c
Pixie <32332642+pixiekidd@users.noreply.github.com>
2018-01-17 19:18:47 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,46 +1,6 @@
 module.exports.run = async (bot,msg,args) =>{
-    const Discord = require('discord.js');
-    const RichEmbed = new Discord.RichEmbed();
-    const prefix = '!';
-
-    let cont = msg.content.slice(prefix.length).split(" ");
-    let argspurge = cont.slice(1);
-
-    msg.delete(9000);    
-
-      async function purge() {
-     
-         
-        if(!msg.member.hasPermission("MANAGE_MESSAGES")){
-            var embed = new Discord.RichEmbed()
-            .setTitle('Error')
-            .setDescription('Error you dont have the required perms - **manage Messages**');
-
-            var msgSent =  msg.channel.send(embed);
-           return;
-        }
-
-        if(isNaN(argspurge[0])){
-         var embed = new Discord.RichEmbed()
-          .setTitle('Error')
-          .setDescription('Error-please only type in a number');
-
-        var msgSent =  msg.channel.send(embed);
-        
-         return;
-        }
-        
-       
-        const fetched = await msg.channel.fetchMessages({limit: argspurge[0]});
-      
-       msg.channel.bulkDelete(fetched)
-       .catch(error=>{msg.channel.send('```js\n Error-'+error+'\n ```')});
-     
-    }
-      
-      purge();
-      
-     
+ 
+    
 }
     
     module.exports.config = {
