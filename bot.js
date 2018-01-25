1d0fcfec4c355e5d59a9d12d82d95c8c8484a319
Pixie <32332642+pixiekidd@users.noreply.github.com>
2018-01-25 18:17:26 +0530

    added welcome and leave messages

--- bot.js
+++ bot.js
@@ -4,31 +4,14 @@ const RichEmbed = new Discord.RichEmbed();
 var fs = require('fs');
 
 // -->COMMANDS<--
-bot.commands = new Discord.Collection();
-fs.readdir('./commands/',(err,files) =>{
 
-    if(err) console.error(err);
-     var jsfiles = files.filter(f => f.split('.').pop()==='js');
-     if(jsfiles.length<= 0){console.log('No commands found');}
-     else{console.log(jsfiles.length + 'founded');}
+bot.on('guildMemberAdd',member =>{
 
-     jsfiles.forEach((f,i) =>{
-         var cmds = require(`./commands/${f}`);
-         console.log(`command ${f} loading....`);
-         bot.commands.set(cmds.config.command,cmds);
-     })
-})
+});
 
 
-bot.on('message',msg =>{
-var prefix = '!';
-var cont = msg.content.slice(prefix.length).split(" ");
-var args = cont.slice(1);
-
-if(!msg.content.startsWith(prefix)) return;
-
-var cmd = bot.commands.get(cont[0]);
-if(cmd) cmd.run(bot,msg,args);
+bot.on('guildMemberRemove', member=>{
+   
 });
 
 
